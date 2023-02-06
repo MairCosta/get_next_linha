0b9b59a9a792cfe7f79b2446122fb9a1b95739dc
Mair Costa <mair.costaa@gmail.com>
2023-02-06 18:54:16 +0000

    20230206

---
+++ get_next_line_bonus.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mair <mair@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/06 18:13:43 by mair              #+#    #+#             */
+/*   Updated: 2023/02/06 18:52:21 by mair             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+char    *get_next_line(int fd);
+
+char	*ft_strjoin(char *line, char *buff);
+
+int     ft_strlen(const char *s);
+
+int		ft_check(char *buf);
+
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mair <mair@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/06 18:23:10 by mair              #+#    #+#             */
+/*   Updated: 2023/02/06 18:50:37 by mair             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int     ft_strlen(char *s)
+{
+    int i;
+
+    i = 0;
+    if (!s)
+        return (0);
+    while (s[i])
+        i++;
+    return(i);
+}
+
+char    *ft_strjoin(char *s1, char *s2)
+{
+    char    *s3;
+    int     i;
+    int     j;
+
+    i = 0;
+    j = 0;
+    if (s2[0] == '\0')
+        return (0);
+    s3 = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+    if (!s3)
+        return (NULL);
+    while (s1 && s1[i])
+    {
+        s3[i] == s1[i];
+        i++;
+    }
+    free (s1);
+    while (s2 && s2[j] && s2[j] != '\n')
+        s3[i++] = s2[j++];
+    if (s2[j] == '\n')
+        s3[i++] = '\n';
+    s3[i] = '\0';
+    return (s3);
+}
+
+int ft_check(char *buff)
+{
+    int i;
+    int j;
+    int check;
+
+    i = 0;
+    j = 0;
+    check = 0;
+    if (!buff)
+        return (0);
+    while (buff[i])
+    {
+        if (check == 1)
+        {
+            buff[j] = buff[i];
+            j++;
+        }
+        if (buff[i] == '\n')
+            check = 1;
+        buff[i] = '\0';
+        i++;
+    }
+    return (check);
+}
+    
